25fa7cf48b02cb608f7a0dcb90c68e6518d7fa50
Nokvix <igor.gass2003@mail.ru>
2025-01-23 18:11:18 +0500

    Реализовал парсинг данных игроков из файлов формата swm

---
+++ file_data_processing.py
@@ -0,0 +1,121 @@
+from typing import Dict
+import re
+
+from path import PATH_TO_FILE
+
+"""
+Код обрабатывает файлы формата smw
+"""
+
+
+class Player:
+    """
+    Объект игрока:
+    name - ФИО игрока;
+    rating - Рейтинг игрока;
+    start_position - Стартовый номер игрока;
+    player_id - Идентификационный номер игрока;
+    points - Количество очков, набранных игроком в турнире;
+    results - Словарь результатов партий:
+    key - стартовый номер соперника, value - результат встречи (« - ничья)
+    """
+
+    def __init__(self, name, rating, start_position, player_id, points, results):
+        self.name = name
+        self.rating = rating
+        self.start_position = start_position
+        self.player_id = player_id
+        self.points = points
+        self.results = results
+
+    def __str__(self):
+        return (f'name: {self.name}\nrating: {self.rating}\nstart_position: {self.start_position}\n'
+                f'player_id: {self.player_id}\npoints: {self.points}\nresults: {self.results}')
+
+
+def create_player(player_str: str) -> Player:
+    """
+    Функция создания объекта игрока
+
+    :param player_str: строка данных игрока из файла
+    :return: объект игрока
+    """
+    player_data = parse_data(player_str)
+    results = reformat_results(player_data['results'])
+    player = Player(
+        name=player_data['name'],
+        rating=player_data['rating'],
+        start_position=player_data['start_position'],
+        player_id=player_data['id'],
+        points=player_data['points'],
+        results=results
+    )
+
+    return player
+
+
+def reformat_results(results: str) -> Dict[int, int]:
+    """
+    Функция преобразования результатов партий
+
+    :param results: результаты партий из файла
+    :return: словарь результатов. key - номер соперника, value - результат
+    """
+    pattern = re.compile(
+        r"(?P<board_number>\d+)"
+        r"(?P<color>[WB])"
+        r"(?P<result>[\d«])#"
+        r"(?P<opponent_number>\d+)"
+    )
+
+    results_dict = {}
+    for result_match in pattern.finditer(results):
+        result_groups = result_match.groupdict()
+        results_dict[result_groups['opponent_number']] = result_groups['result']
+
+    return results_dict
+
+
+def parse_data(player_str: str) -> Dict[str, str]:
+    """
+    Функция разбиения данных
+
+    :param player_str: строка данных игрока из файла
+    :return: словарь с данными игрока
+    """
+    pattern = re.compile(
+        r"^(?P<current_position>\d+)\s+"
+        r"(?P<start_position>\d+)\s+"
+        r"(?P<name>[a-zA-Zа-яА-ЯёЁ\s]+)\s+"
+        r"(?P<rating>\d+)?\**\s+"
+        r"(?P<title>[A-Z]*)\s+"
+        r"(?P<id>\d+)?\s+"
+        r"(?P<points>[0-9«]+)\s+"
+        r"(?P<first_color>[WB])\s+"
+        r"(?P<results>.+)$"
+    )
+
+    player_data = pattern.fullmatch(player_str)
+    player_groups = player_data.groupdict()
+
+    return player_groups
+
+
+def main():
+    with open(PATH_TO_FILE, 'r') as file:
+        split_data = file.readline().split()
+        number_players, number_tours = map(int, [split_data[0], split_data[1]])
+        file_lines = file.readlines()
+
+    players = {}
+    for i in range(number_players):
+        player = create_player(file_lines[i].strip())
+        players[player.start_position] = player
+
+    for player in players.values():
+        print(player)
+        print()
+
+
+if __name__ == '__main__':
+    main()
